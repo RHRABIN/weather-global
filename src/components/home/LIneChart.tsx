1c77754af0fb8b995d216c8e6b0b7a0765458d49
RH Rabin <97010308+RHRABIN@users.noreply.github.com>
2023-11-18 18:36:39 +0600

    Update LIneChart loading

--- src/components/home/LIneChart.tsx
+++ src/components/home/LIneChart.tsx
@@ -8,7 +8,7 @@ const LIneChart: React.FC<any> = ({ data, forecastLoading }) => {
     const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
 
     let content = null
-    if (!forecastLoading) {
+    if (forecastLoading) {
         content = <div className='h-full w-full border rounded  mt-2 bg-gray-300 animate-pulse'></div>
     } else {
         data?.list?.splice(0, 5)?.map((i: any, idx: number) => chartData.push({ name: forecastDays[idx], weather: Math.round(i?.main?.temp) }));
